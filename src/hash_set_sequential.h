02c91c3ce42b41ddb67566d89d662c6db566b44f
Eli Gilbert <eg1923@ic.ac.uk>
2025-11-06 10:39:52 +0000

    refactor: remove unnecessary functions and members

--- src/hash_set_sequential.h
+++ src/hash_set_sequential.h
@@ -14,9 +14,8 @@ template <typename T> class HashSetSequential : public HashSetBase<T> {
   using bucket_t = std::vector<T>;
 
 public:
-  explicit HashSetSequential(size_t initial_buckets = 16)
-      : n_buckets_(initial_buckets) {
-    buckets_ = std::vector<bucket_t>(n_buckets_, bucket_t());
+  explicit HashSetSequential(size_t initial_buckets = 16) {
+    buckets_ = std::vector<bucket_t>(initial_buckets, bucket_t());
   }
 
   // Adds |elem| to the hash set. Returns true if |elem| was absent, and false
@@ -30,7 +29,7 @@ public:
     }
 
     // Resize if load factor exceeded
-    if (size_ + 1 >= kResizeThreshold * n_buckets_) {
+    if (size_ + 1 >= kResizeThreshold * buckets_.size()) {
       Resize();
       bucket = &GetBucket(elem);
     }
@@ -55,7 +54,7 @@ public:
   }
 
   // Returns true if |elem| is present in the hash set, and false otherwise.
-  bool Contains(T elem) const override {
+  bool Contains(T elem) override {
     auto &bucket = GetBucket(elem);
     return GetIndex(bucket, elem) != bucket.end();
   }
@@ -65,17 +64,16 @@ public:
 
 private:
   size_t size_ = 0;
-  size_t n_buckets_;
   std::vector<bucket_t> buckets_;
 
   // Redistributes the elements in the hash set into a larger number of buckets.
   void Resize() {
-    n_buckets_ *= kCountResize;
-    std::vector<bucket_t> new_buckets(n_buckets_, bucket_t());
+    size_t new_size = buckets_.size() * kCountResize;
+    std::vector<bucket_t> new_buckets(new_size, bucket_t());
 
     for (const auto &bucket : buckets_) {
       for (const auto &elem : bucket) {
-        new_buckets[std::hash<T>()(elem) % n_buckets_].push_back(elem);
+        new_buckets[std::hash<T>()(elem) % new_size].push_back(elem);
       }
     }
 
@@ -84,12 +82,7 @@ private:
 
   // Returns the bucket index for the given element (mutable version).
   bucket_t &GetBucket(const T &elem) {
-    return buckets_[std::hash<T>()(elem) % n_buckets_];
-  }
-
-  // Returns the bucket index for the given element (const version).
-  const bucket_t &GetBucket(const T &elem) const {
-    return buckets_[std::hash<T>()(elem) % n_buckets_];
+    return buckets_[std::hash<T>()(elem) % buckets_.size()];
   }
 
   // Gets the index of an item in a list

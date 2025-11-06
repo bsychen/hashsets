91f65133bbf8bb3e3d2facdc7d05789febc22f0e
bsychen <85930791+bsychen@users.noreply.github.com>
2025-11-06 16:29:55 +0000

    feat: coarse grained works

--- src/hash_set_coarse_grained.h
+++ src/hash_set_coarse_grained.h
@@ -1,29 +1,33 @@
 #ifndef HASH_SET_COARSE_GRAINED_H
 #define HASH_SET_COARSE_GRAINED_H
 
+#include <algorithm>
 #include <cassert>
-#include <mutex>
+#include <functional>
+#include <mutex>    
+#include <vector>
 
 #include "src/hash_set_base.h"
 
 template <typename T> class HashSetCoarseGrained : public HashSetBase<T> {
+  using Bucket = std::vector<T>;
+  static constexpr double kResizeThreshold = 0.75;
+  static constexpr size_t kCountResize = 2;
+
 public:
   explicit HashSetCoarseGrained(size_t initial_buckets = 16) {
     assert(initial_buckets > 0);
-    buckets_ = std::vector<bucket_t>(initial_buckets, bucket_t());
+    buckets_ = std::vector<Bucket>(initial_buckets, Bucket());
   }
 
   bool Add(T elem) override {
-    bucket_t *bucket = &GetBucket(elem);
+    std::scoped_lock<std::mutex> lock(mutex_);
 
-    if (GetIndex(*bucket, elem) != bucket->end()) {
-      return false;
-    }
+    Bucket *bucket = &GetBucket(elem);
 
-    if (size_ > kResizeThreshold * buckets_.size()) {
-      Resize();
-      bucket = &GetBucket(elem);
-    }
+    if (GetIndex(*bucket, elem) != bucket->end()) { return false; }
+
+    if (Policy()) { Resize(); bucket = &GetBucket(elem); }
 
     bucket->push_back(elem);
     ++size_;
@@ -31,7 +35,9 @@ public:
   }
 
   bool Remove(T elem) override {
-    bucket_t &bucket = GetBucket(elem);
+    std::scoped_lock<std::mutex> lock(mutex_);
+    
+    Bucket &bucket = GetBucket(elem);
     auto it = GetIndex(bucket, elem);
     if (it != bucket.end()) {
       bucket.erase(it);
@@ -42,14 +48,46 @@ public:
   }
 
   bool Contains(T elem) override {
+    std::scoped_lock<std::mutex> lock(mutex_);
     auto &bucket = GetBucket(elem);
     return GetIndex(bucket, elem) != bucket.end();
   }
 
-  size_t Size() const override { return size_; }
+  size_t Size() const override { 
+    std::scoped_lock<std::mutex> lock(mutex_);
+    return size_; 
+  }
 
 private:
-  std::mutex mutex_;
+  mutable std::mutex mutex_;
+  size_t size_ = 0;
+  std::vector<Bucket> buckets_;
+
+  bool Policy() const {
+    return static_cast<double>(size_) / static_cast<double>(buckets_.size()) > kResizeThreshold;
+  }
+
+  void Resize() {
+    size_t new_size = buckets_.size() * kCountResize;
+    std::vector<Bucket> new_buckets(new_size, Bucket());
+
+    for (const auto &bucket : buckets_) {
+      for (const auto &elem : bucket) {
+        new_buckets[std::hash<T>()(elem) % new_size].push_back(elem);
+      }
+    }
+
+    buckets_ = std::move(new_buckets);
+  }
+
+  Bucket &GetBucket(const T &elem) {
+    return buckets_[std::hash<T>()(elem) % buckets_.size()];
+  }
+
+  typename Bucket::const_iterator GetIndex(const Bucket &list,
+                                             const T &item) const {
+    return std::find(list.begin(), list.end(), item);
+  }
 };
 
 #endif  // HASH_SET_COARSE_GRAINED_H
--- src/hash_set_sequential.h
+++ src/hash_set_sequential.h
@@ -12,29 +12,26 @@ const size_t kResizeThreshold = 3;
 const size_t kCountResize = 2;
 
 template <typename T> class HashSetSequential : public HashSetBase<T> {
-  using bucket_t = std::vector<T>;
+  using Bucket = std::vector<T>;
 
 public:
   explicit HashSetSequential(size_t initial_buckets = 16) {
     assert(initial_buckets > 0);
-    buckets_ = std::vector<bucket_t>(initial_buckets, bucket_t());
+    buckets_ = std::vector<Bucket>(initial_buckets, Bucket());
   }
 
   // Adds |elem| to the hash set. Returns true if |elem| was absent, and false
   // otherwise.
   bool Add(T elem) override {
     // Short circuit if elem already present
-    bucket_t *bucket = &GetBucket(elem);
+    Bucket *bucket = &GetBucket(elem);
 
     if (GetIndex(*bucket, elem) != bucket->end()) {
       return false;
     }
 
     // Resize if load factor exceeded
-    if (size_ > kResizeThreshold * buckets_.size()) {
-      Resize();
-      bucket = &GetBucket(elem);
-    }
+    if (Policy()) { Resize(); bucket = &GetBucket(elem); }
 
     // Add element
     bucket->push_back(elem);
@@ -45,7 +42,7 @@ public:
   // Removes |elem| from the hash set. Returns true if |elem| was present, and
   // false otherwise.
   bool Remove(T elem) override {
-    bucket_t &bucket = GetBucket(elem);
+    Bucket &bucket = GetBucket(elem);
     auto it = GetIndex(bucket, elem);
     if (it != bucket.end()) {
       bucket.erase(it);
@@ -66,12 +63,16 @@ public:
 
 private:
   size_t size_ = 0;
-  std::vector<bucket_t> buckets_;
+  std::vector<Bucket> buckets_;
+
+  bool Policy() const {
+    return size_ > buckets_.size() * kResizeThreshold;
+  }
 
   // Redistributes the elements in the hash set into a larger number of buckets.
   void Resize() {
     size_t new_size = buckets_.size() * kCountResize;
-    std::vector<bucket_t> new_buckets(new_size, bucket_t());
+    std::vector<Bucket> new_buckets(new_size, Bucket());
 
     for (const auto &bucket : buckets_) {
       for (const auto &elem : bucket) {
@@ -83,12 +84,12 @@ private:
   }
 
   // Returns the bucket index for the given element (mutable version).
-  bucket_t &GetBucket(const T &elem) {
+  Bucket &GetBucket(const T &elem) {
     return buckets_[std::hash<T>()(elem) % buckets_.size()];
   }
 
   // Gets the index of an item in a list
-  typename bucket_t::const_iterator GetIndex(const bucket_t &list,
+  typename Bucket::const_iterator GetIndex(const Bucket &list,
                                              const T &item) const {
     return std::find(list.begin(), list.end(), item);
   }
